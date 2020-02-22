d1e6c13cee7ba655a9dd9c7f1f7f18e774b963c0
Düzgün <duzguntutar@gmail.com>
2020-02-22 23:38:54 +0300

    first commit

---
+++ Ttar/Ttar.Business/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ttar.Business
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Ttar/Ttar.Entities/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ttar.Entities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
