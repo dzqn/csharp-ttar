007784b8ba70c139865b1438cf154291897b1ac6
Düzgün <duzguntutar@gmail.com>
2020-06-25 00:07:15 +0300

    product map

---
+++ Ttar.DAL/Ef/Concrete/Mapping/ProductMap.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Ttar.Entities.Concrete;
+
+namespace Ttar.DAL.Concrete.Mapping
+{
+    public class ProductMap : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable(@"Products", @"dbo");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).HasColumnName("Id");
+            builder.Property(x => x.Name).HasColumnName("Name");
+            builder.Property(x => x.QuantityPerUnit).HasColumnName("QuantityPerUnit");
+            builder.Property(x => x.UnitPrice).HasColumnName("UnitPrice");
+        }
+    }
+}
